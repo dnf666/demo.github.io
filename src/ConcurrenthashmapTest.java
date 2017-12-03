abdea85082bb2bde867e00a61d60c6e9e709575d
dnf666 <1204695257@qq.com>
2017-12-03 17:31:02 +0800

    先保存下

---
+++ src/ConcurrenthashmapTest.java
@@ -0,0 +1,7 @@
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrenthashmapTest {
+    public static void main(String[] args) {
+        ConcurrentHashMap<String,String> concurrentHashMap = new ConcurrentHashMap<>();
+    }
+}
